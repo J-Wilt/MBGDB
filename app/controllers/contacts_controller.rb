99be60720fb58b336a4e562237726affaaff7547
Jason Wilt <wilt.jason@gmail.com>
2020-02-27 02:21:41 +0000

    Finished contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,8 +6,9 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new(contact_params)
         if @contact.save 
-            redirect_to new_contract_path, notice: "Message Sent."
-        else redirect_to new_contract_path, notice: "Error Occured"
+            redirect_to new_contact_path, notice: "Message Sent."
+        else 
+            redirect_to new_contact_path, notice: "Error Occured"
         end 
     end 
 
