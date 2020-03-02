aa5121681e750fb79036e49721c34aa19cc3df26
Jason Wilt <wilt.jason@gmail.com>
2020-03-02 21:17:09 +0000

    Added User Index Page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+ before_action :authenticate_user! 
+  before_action :only_current_user
+
   # GET to /users/:user_id/profile/new
   def new
     @profile = Profile.new
@@ -17,9 +20,37 @@ def create
     render action: :new
   end
 end
+
+#GET request made to users/:user_id/profile/edit
+def edit
+  @user = User.find( params[:user_id]) 
+  @profile = @user.profile
+end
+
+#PUSH and PUT for user/:user_id/profile
+def update
+  @user = User.find( params[:user_id]) 
+  @profile = @user.profile
+  if  @profile.update_attributes(profile_params)
+    flash[:success]= "Profile Successfully Updated!"
+    redirect_to user_path(id: params[:user_id])
+  else
+    render action :edit
+  end
+end
+
 private
+
+
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :avatar, :city, :state, :zip, :contact_email, :description)
   end
+  
+  def only_current_user
+    @user = User.find( params[:user_id] )
+    redirect_to(root_url) unless @user == current_user
+  end  
+  
+  
   
 end
