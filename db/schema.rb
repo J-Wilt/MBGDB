0b90db7839a78d3f25577079a5d046c0610f88b1
EC2 Default User <ec2-user@ip-172-31-17-142.us-east-2.compute.internal>
2020-03-04 01:41:58 +0000

    rebuild after breakdown

--- db/schema.rb
+++ db/schema.rb
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20200301060806) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "user_id"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "plan_id"
